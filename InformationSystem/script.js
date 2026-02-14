9f25462c133e1477eb32b05b8715f45acaa996f0
Clarisa Garcia <garciaclarisa.t@gmail.com>
2026-02-14 11:53:01 +0800

    Delete InformationSystem directory

--- InformationSystem/script.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click','#btnDelete', function (e)
-{
-    e.preventDefault();
-    if(confirm("Are you sure to delete this item?")){
-        console.log("Sumakses syaaaaa!!!!");
-    }
-});
